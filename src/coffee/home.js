086efa1cb93c033bbbcf577d20bcd413583c98bb
C-Mahendran <mahendranc@0414@gmail.com>
2025-02-26 20:14:32 +0530

    Initial commit

--- src/coffee/home.js
+++ src/coffee/home.js
@@ -19,6 +19,28 @@ import MenunonMilkTea from './img/menu non Milk Tea.webp';
 
 
 export default function home() {
+
+  let menus = [
+    {name:"Cofee",img: Menuamericano, price:"50.00 rupee"},
+    {name:"Cofee2",img:Menucappuccino, price:"50.00 rupee"},
+    {name:"Cofee3",img:MenuCaffeLatte, price:"50.00 rupee"},
+    {name:"Cofee4",img:Menufrappuccino, price:"50.00 rupee"},
+    {name:"Cofee5",img:Menuespresso, price:"50.00 rupee"},
+    {name:"Cofee6",img:Menumacchiato, price:"50.00 rupee"},
+    {name:"Cofee7",img:Menucaramellatte, price:"50.00 rupee"},
+    {name:"Cofee8",img:MenuaffogatoCoffee, price:"50.00 rupee"},
+  ]
+
+  let menus2 = [
+    {name:"Cofee9",img: MenunonChamomile, price:"25.00 rupee"},
+    {name:"Cofee9",img:  MenunonBlackTea, price:"25.00 rupee"},
+    {name:"Cofee9",img: MenunonEarlGrey, price:"25.00 rupee"},
+    {name:"Cofee9",img: MenunonEnglishBreakfast, price:"25.00 rupee"},
+    {name:"Cofee9",img: MenunonGreenTea, price:"25.00 rupee"},
+    {name:"Cofee9",img: MenunonJasmineTea, price:"25.00 rupee"},
+    {name:"Cofee9",img: MenunonLycheeTea, price:"25.00 rupee"},
+    {name:"Cofee9",img:MenunonMilkTea , price:"25.00 rupee"},
+  ]
   return (
  <div>
   <div className='my-component'>
@@ -39,25 +61,44 @@ export default function home() {
         <div className="col-lg">
           <h3>Coffee</h3>
           <div className=''>
-          <p><img style={{width:'100px',borderRadius:'50%'}} src={ Menuamericano} />Americano <span className="price">-------------50.00 Rs</span></p>
+            {menus && menus.map((menu)=>{
+              return(
+                <tr>
+                  <td><img style={{width:'100px',height:"100px",objectFit:"cover",marginBottom:"20px",borderRadius:'50%'}} src={menu.img} /></td>
+                  <td style={{paddingLeft:"20px"}}>{menu.name}</td>
+                  <td style={{  color:" #f39c12"}} >-------------{menu.price}</td>
+                </tr>
+              )
+            })}
+          {/* <p><img style={{width:'100px',borderRadius:'50%'}} src={ Menuamericano} />Americano <span className="price">-------------50.00 Rs</span></p>
           <p><img style={{width:'100px',borderRadius:'50%'}} src={ Menucappuccino} />Cappuccino <span className="price">-------------50.00 Rs</span></p>
           <p><img style={{width:'100px',borderRadius:'50%'}} src={ MenuCaffeLatte} />Caffe Latte <span className="price">-------------50.00 Rs</span></p>
           <p><img style={{width:'100px',height:'80px',borderRadius:'50%'}} src={ Menufrappuccino} />Frappuccino <span className="price">-------------50.00 Rs</span></p>
           <p><img style={{width:'100px',borderRadius:'50%'}} src={ Menumacchiato} />Macchiato <span className="price">-------------50.00 Rs</span></p>
           <p><img style={{width:'100px',borderRadius:'50%'}} src={ Menuespresso} />Espresso <span className="price">-------------50.00 Rs</span></p>
-          <p><img style={{width:'100px',borderRadius:'50%',height:'80PX'}} src={ Menucaramellatte} />Caramel Latte <span className="price">-------------50.00 Rs</span></p>
+          <p><img style={{width:'100px',borderRadius:'50%',height:'80PX'}} src={ Menucaramellatte} />Caramel Latte <span className="price">-------------40.00 Rs</span></p>
           <p><img style={{width:'100px',borderRadius:'50%'}} src={ MenuaffogatoCoffee} />Affogato Coffee <span className="price">-------------50.00 Rs</span></p>
+         */}
         </div></div>
 <div className="col-lg"> 
           <h3>NoN Coffee</h3>
-          <p><img style={{width:'100px',borderRadius:'50%'}} src={ MenunonChamomile } />Chamomile <span className="price">-------------25.00 Rs</span></p>
+          {menus2 && menus2.map((menu)=>{
+              return(
+                <tr>
+                  <td><img style={{width:'100px',height:"100px",objectFit:"cover",marginBottom:"20px",borderRadius:'50%' ,color:" #f39c12"}} src={menu.img} /></td>
+                  <td style={{paddingLeft:"20px"}}>{menu.name}</td>
+                  <td style={{  color:" #f39c12"}}>-------------{menu.price}</td>
+                </tr>
+              )
+            })}
+          {/* <p><img style={{width:'100px',borderRadius:'50%'}} src={ MenunonChamomile } />Chamomile <span className="price">-------------25.00 Rs</span></p>
           <p><img style={{width:'100px',borderRadius:'50%'}} src={ MenunonBlackTea } /> Black Tea<span className="price">-------------25.00 Rs</span></p>
           <p><img style={{width:'100px',borderRadius:'50%'}} src={ MenunonEarlGrey } />Earl Grey <span className="price">-------------25.00 Rs</span></p>
           <p><img style={{width:'100px',borderRadius:'50%'}} src={ MenunonEnglishBreakfast }/> Breakfast<span className="price">-------------25.00 Rs</span></p>
           <p><img style={{width:'100px',borderRadius:'50%'}} src={ MenunonGreenTea } />Green Tea <span className="price">-------------25.00 Rs</span></p>
           <p><img style={{width:'100px',height:'80px',borderRadius:'50%'}} src={ MenunonJasmineTea } />Jasmine Tea <span className="price">-------------25.00 Rs</span></p>
           <p><img style={{width:'100px',height:'80px',borderRadius:'50%'}} src={ MenunonLycheeTea } />Lychee Tea <span className="price">-------------25.00 Rs</span></p>
-          <p><img style={{width:'100px',borderRadius:'50%'}} src={ MenunonMilkTea } />Milk Tea <span className="price">-------------25.00 Rs</span></p>
+          <p><img style={{width:'100px',borderRadius:'50%'}} src={ MenunonMilkTea } />Milk Tea <span className="price">-------------25.00 Rs</span></p> */}
         </div>
       </div>
     </div>
